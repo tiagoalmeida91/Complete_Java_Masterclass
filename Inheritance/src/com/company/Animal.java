bef0345a305506c47cd5f4d7a9ea670c4b7659cd
tiagoalmeida91 <31996413+tiagoalmeida91@users.noreply.github.com>
2017-10-02 16:02:17 +0100

    the dog got feeded

--- Inheritance/src/com/company/Animal.java
+++ Inheritance/src/com/company/Animal.java
@@ -1,31 +1,43 @@
 package com.company;
 
 public class Animal {
+
+    private String name;
     private int brain;
     private int body;
     private int size;
     private int weight;
 
-    public Animal(private int brain, private int body, private int size, private int weight) {
+    public Animal( String name, int brain,  int body,  int size,  int weight) {
         this.brain = brain;
         this.body = body;
         this.size = size;
         this.weight = weight;
     }
 
-    public private int getBrain() {
+    public void eat(){
+        System.out.println("Animal.eat() called");
+    }
+
+    public void move(){
+
+    }
+
+
+
+    public  int getBrain() {
         return brain;
     }
 
-    public private int getBody() {
+    public  int getBody() {
         return body;
     }
 
-    public private int getSize() {
+    public  int getSize() {
         return size;
     }
 
-    public private int getWeight() {
+    public  int getWeight() {
         return weight;
     }
 }
