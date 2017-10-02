210327d798bf4c7a9a934d4bf112675d462afd86
tiagoalmeida91 <31996413+tiagoalmeida91@users.noreply.github.com>
2017-10-02 15:38:42 +0100

    Added Animal and dog classes

---
+++ Inheritance/src/com/company/Animal.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class Animal {
+    private_int brain;
+    private_int body;
+    private_int size;
+    private_int weight;
+
+    public Animal(private_int brain, private_int body, private_int size, private_int weight) {
+        this.brain = brain;
+        this.body = body;
+        this.size = size;
+        this.weight = weight;
+    }
+
+    public private_int getBrain() {
+        return brain;
+    }
+
+    public private_int getBody() {
+        return body;
+    }
+
+    public private_int getSize() {
+        return size;
+    }
+
+    public private_int getWeight() {
+        return weight;
+    }
+}
---
+++ Inheritance/src/com/company/Dog.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Dog extends Animal{
+
+    public Dog(private_int brain, private_int body, private_int size, private_int weight) {
+        super(brain, body, size, weight);
+    }
+}
