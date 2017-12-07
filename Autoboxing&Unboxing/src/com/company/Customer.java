cbc66bb497a258921397fea025ef529e50bb71b9
tiagoalmeida91 <31996413+tiagoalmeida91@users.noreply.github.com>
2017-12-07 17:56:56 +0000

    Autoboxing&Unboxing_challenge

---
+++ Autoboxing&Unboxing/src/com/company/Customer.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Customer {
+
+    private String name;
+    private ArrayList<Double>transaction;
+
+    public Customer(String name, double initialAmount){
+        this.name=name;
+        this.transaction=new ArrayList<Double>();
+        addTransaction(initialAmount);
+    }
+
+    public void addTransaction(double amount){
+        this.transaction.add(amount);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Double> getTransaction() {
+        return transaction;
+    }
+}
